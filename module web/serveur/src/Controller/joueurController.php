8d655160eba029c1006b81fcdb908dbde55e60f1
Naël <nael.juston@etu.univ-lyon1.fr>
2022-01-21 14:28:49 +0100

    Can now require the player list

---
+++ module web/serveur/src/Controller/joueurController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Joueur;
+use App\Repository\JoueurRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class joueurController extends AbstractController
+{
+    public function recupJoueur(ManagerRegistry $doctrine) : Response
+    {
+        /** @var JoueurRepository $joueurRepository */
+        $joueurRepository=$doctrine->getRepository(Joueur::class);
+        $joueurs=$joueurRepository->findAll();
+        
+        $rows=[];
+        foreach($joueurs as $un_joueur) {
+            $rows[] = [
+                'Prenom' => (string)$un_joueur->getPrenomjoueur(),
+                'Nom' => (string)$un_joueur->getNomjoueur(),
+            ];
+        }
+        echo json_encode($rows);
+        return new Response("ok");
+    }
+
+
+    
+
+    
+  
+}
