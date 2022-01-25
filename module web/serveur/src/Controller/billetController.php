a4323b16e0169a9c7b68d04fd12fd6aaa9091740
Naël <nael.juston@etu.univ-lyon1.fr>
2022-01-25 11:59:16 +0100

    fix

--- module web/serveur/src/Controller/billetController.php
+++ module web/serveur/src/Controller/billetController.php
@@ -32,20 +32,62 @@ class billetController extends AbstractController
 
     public function getById(ManagerRegistry $doctrine, int $billetId) : Response
     {
+        /** @var JouerRepository $jouerRepository */
+        $jouerRepository=$doctrine->getRepository(Jouer::class);
+        
+        /** @var JoueurRepository $joueurRepository */
+        $joueurRepository=$doctrine->getRepository(Joueur::class);
+        
         /** @var BilletRepository $billetRepository */
         $billetRepository=$doctrine->getRepository(Billet::class);
         $billet=$billetRepository->findById($billetId);
         
-         /** @var MatchsRepository $matchRepository */
-         $matchRepository=$doctrine->getRepository(Matchs::class);
-         $day=$billet['jourId'];
-         $court=$billet['courtId'];
-         $matchs=$matchRepository->findByDayCourt($day,$court);
+        /** @var MatchsRepository $matchRepository */
+        $matchRepository=$doctrine->getRepository(Matchs::class);
+        $day=$billet[0]->getJourId();
+        $court=$billet[0]->getCourtId();
+        $matchs=$matchRepository->findByDayCourt($day,$court);
+        $liste_matchs=[];
+        $i=0;
+        foreach($matchs as $match){
+            $liste_matchs[$i]=[
+                'matchid'=>(int)$match->getMatchId(),
+                'matchtype'=>(int)$match->getTypetournoiid(),
+                'matchcourtid'=>(int)$match->getCourtId(),
+                'matchtrancheid'=>(int)$match->getTrancheid(),
+                'matchcourt'=>(string)'',
+                'joueursId'=>[],
+                'joueursNoms'=>[],
+            ];
+            $i++;
+        }
         
+        $liste_matchs2=[];
+        foreach($liste_matchs as $un_match){
+            $jouers=$jouerRepository->findByMatch($un_match['matchid']);
+            foreach($jouers as $jouer){
+                $id=$jouer->getJoueurId();
+                $un_match['joueursId'][]=$id;
+            }
+            $liste_matchs2[]=$un_match;
+        }
+
+        $liste_matchs3=[];
+        foreach($liste_matchs2 as $un_match){
+            foreach($un_match['joueursId'] as $un_jId){
+                // var_dump($un_jId);die;
+                if($un_jId==NULL){
+                    $un_match['joueursNoms'][]='indefini';
+                }else {
+                    $joueur=$joueurRepository->find($un_jId);                
+                    $nom=$joueur->getNomjoueur();
+                    $un_match['joueursNoms'][]=$nom;
+                }
+            }
+            $liste_matchs3[]=$un_match;
+        }
         
-        $le_billet=[];
-        $le_billet['billet']=$billet;
-        $le_billet['matchs']=$matchs;
+        $le_billet[]=['billet'=>$billet[0],'matchs'=>$liste_matchs3fix];
         $rows=[];
         foreach($le_billet as $un_billet) {
             $rows[] = [
